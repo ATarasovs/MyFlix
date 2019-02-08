c7d28fec419675b14c96ae7e1dffd5788b6fcc64
Aleksandr Tarasov <a.tarasov1209@gmail.com>
2019-02-08 01:39:22 +0000

    implemented nav bar

--- Php-Apache/www/include/header.php
+++ Php-Apache/www/include/header.php
@@ -24,3 +24,24 @@ include '../config/mongo_connect.php';
     <link rel="stylesheet" href="../css/styles.css">
 </head>
 <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">MyFlix</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Videos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                </li class="nav-item right">
+                    <a class="nav-link" href="#">Logout</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
