f9a9b5d056eb7d6d108ef7ecb062ae61b235d279
Aleksandr Tarasov <a.tarasov1209@gmail.com>
2019-02-11 01:00:51 +0000

    implemented advanced filters

--- Php-Apache/www/videos/videoList.php
+++ Php-Apache/www/videos/videoList.php
@@ -7,16 +7,56 @@
      */
 
     include '../include/header.php';
+
     $videoTitle = $_GET['title'];
-    if ($videoTitle == null || $videoTitle == "") {
+    $videoCategory = $_GET['category'];
+
+    if (($videoTitle == null || $videoTitle == "") && ($videoCategory == null || $videoCategory == "")){
         $videos = $videosCollection->find();
     }
-    else {
+    else if ($videoCategory == null || $videoCategory == ""){
         $videos = $videosCollection->find(["name" => $videoTitle]);
     }
+    else if ($videoTitle == null || $videoTitle == ""){
+        $videos = $videosCollection->find(["category" => $videoCategory]);
+    }
+    else {
+        $videos = $videosCollection->find(["name" => $videoTitle, "category" => $videoCategory]);
+    }
+
+    $categories = $categoriesCollection->find();
 ?>
 
 <div class="container">
+    <div class="row align-items-center justify-content-center">
+        <form role="advancedSearch">
+            <div class="col-md-5">
+                <div class="form-group">
+                    <?php if($videoTitle != "") { ?>
+                        <input type="text" class="form-control" name="title" value="<?php echo $videoTitle; ?>">
+                    <?php } else {?>
+                        <input type="text" class="form-control" name="title">
+                    <?php } ?>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <div class="form-group">
+                    <select id="categorySelect" class="form-control" name="category">
+                        <option value=""></option>
+                        <?php
+                            foreach ($categories as $category) {
+                                echo "<option value='" . $category['category'] . "'>" . $category['category'] . "</option>";
+                            }
+                        ?>
+                    </select>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <button type="submit" class="btn btn-primary btn-block submit"><i class="fa fa-search"></i> Search</button>
+            </div>
+        </form>
+    </div>
+
     <ul class="list-unstyled video-list-thumbs row">
         <?php foreach ($videos as $video) { ?>
             <li class="col-lg-4 col-sm-6 col-xs-12">
@@ -30,6 +70,14 @@
         <?php } ?>
     </ul>
 </div>
-<table>
 
+<script>
+    var category = "<?php echo $videoCategory ?>";
+
+    $(document).ready(function() {
+        if (category != "") {
+            $("#categorySelect").val(category);
+        }
+    });
+</script>
 <?php include '../include/footer.php'; ?>
