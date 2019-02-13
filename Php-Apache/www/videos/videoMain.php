42cd02e8f9bf8c0ce90481a2cb41391c9aa9ec6d
Aleksandr Tarasov <a.tarasov1209@gmail.com>
2019-02-13 20:12:55 +0000

    improved slideshow on the home page

--- Php-Apache/www/videos/videoMain.php
+++ Php-Apache/www/videos/videoMain.php
@@ -9,6 +9,7 @@
 include '../include/header.php';
 
 $videos = $videosCollection->find();
+$videos = $videos->sort(array('rating'=>-1))->limit(3);
 $count = 0;
 ?>
 
@@ -19,6 +20,7 @@ $count = 0;
             <ol class="carousel-indicators">
                 <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
                 <li data-target="#myCarousel" data-slide-to="1"></li>
+                <li data-target="#myCarousel" data-slide-to="2"></li>
             </ol>
 
             <!-- Wrapper for slides -->
